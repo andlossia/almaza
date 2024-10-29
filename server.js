eeeda51f92938b2808f4fa33d235ac89352b6882
Andlossia <69584141+andlossia@users.noreply.github.com>
2024-10-29 05:17:08 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -12,10 +12,13 @@ const corsOptions = {
   origin: process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(',') : '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
+  credentials: true // Add this line if your frontend requires credentials like cookies or auth headers
 };
-app.options('*', cors(corsOptions)); 
 
 app.use(cors(corsOptions));
+app.options('*', cors(corsOptions)); 
+
+
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(responseHandler);
