92e3fa10dd674ca8b07bcf04d81d33c2caedbd52
Andlossia <69584141+andlossia@users.noreply.github.com>
2024-11-17 08:06:42 +0200

    Add files via upload

--- server.js
+++ server.js
@@ -2,43 +2,27 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const { connectToDatabase } = require('./database');
+const {connectToDatabase} = require('./database');
 const responseHandler = require('./middlewares/handlingMiddleware');
 
 dotenv.config();
 const app = express();
 
 const corsOptions = {
-  origin: (origin, callback) => {
-    const allowedOrigins = process.env.CORS_ORIGIN
-      ? process.env.CORS_ORIGIN.split(',')
-      : [];
-    
-    allowedOrigins.push('http://localhost:4200'); 
-
-    if (!origin || allowedOrigins.includes(origin)) {
-      callback(null, true);
-    } else {
-      callback(new Error('Not allowed by CORS'));
-    }
-  },
+  origin: process.env.CORS_ORIGIN ? process.env.CORS_ORIGIN.split(',') : '*',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'],
   allowedHeaders: ['Origin', 'X-Requested-With', 'Content-Type', 'Accept', 'Authorization'],
-  credentials: true,
+  credentials: true // Add this line if your frontend requires credentials like cookies or auth headers
 };
-
 
 app.use(cors(corsOptions));
 app.options('*', cors(corsOptions)); 
 
-app.use((req, res, next) => {
-  req.setTimeout(300000);
-  next();
-});
 
-app.use(bodyParser.json({ limit: '1024mb' }));
-app.use(bodyParser.urlencoded({ limit: '1024mb', extended: true }));
+app.use(bodyParser.json({ limit: '50mb' }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 app.use(responseHandler);
+
 
 app.use('/', require('./routes/media'));
 app.get('/', (req, res) => {
@@ -47,24 +31,23 @@ app.get('/', (req, res) => {
 
 app.use('/api/v1', require('./routes/router'));
 
-app.use((err, req, res, next) => {
-  if (err instanceof Error && err.message === 'Not allowed by CORS') {
-    return res.status(403).json({ message: 'CORS policy violation.' });
-  }
-  next(err);
-});
-
 const startServer = async () => {
   try {
     await connectToDatabase();
     const port = process.env.PORT || 8080;
-    app.listen(port, () => {
-      console.log(`Server running at http://localhost:${port}`);
+    app.listen(port, (err) => {
+      if (err) {
+        console.error(`Error starting server: ${err.message}`);
+        process.exit(1);
+      } else {
+        console.log(`Server running at http://localhost:${port}`);
+      }
     });
   } catch (err) {
     console.error(`Failed to connect to database: ${err.message}`);
     process.exit(1);
   }
 };
+
 
 startServer();
