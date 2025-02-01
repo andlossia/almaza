bd6cd92c603328a1ff56a4ca598aa56c222a5c2d
Andlossia <69584141+andlossia@users.noreply.github.com>
2025-02-01 11:18:12 +0200

    Add files via upload

--- controllers/crud/registerController.js
+++ controllers/crud/registerController.js
@@ -1,7 +1,19 @@
+const mongoose = require('mongoose');
 const initController = require('./initController');
 
-const registerController = (Model, modelName, customMethods = []) => {
-  return initController(Model, modelName, customMethods);
+
+const registerControllers = async () => {
+  try {
+    const collections = await mongoose.connection.db.listCollections().toArray();
+    const controllers = {};
+    collections.forEach(({ name }) => {
+      controllers[name] = initController(name);
+    });
+    return controllers;
+  } catch (error) {
+    throw new Error('Failed to register controllers');
+  }
 };
 
-module.exports = registerController;
+
+module.exports = registerControllers;
